d6a3fd2dbe894fc43208640f609c8981e4f4a94b
St3listu96 <32820467+St3listu96@users.noreply.github.com>
2018-03-13 11:46:53 +0200

    Add files via upload

--- analizor.c
+++ analizor.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+#define SAFEALLOC(var,Type) if((var=(Type*)malloc(sizeof(Type)))==NULL)err("Memorie insuficienta");
 
 char *caracter;
+enum{COMMA,SEMICOLON,LPAR,RPAR,LBRACKET,RBRACKET,LACC,RACC,ADD,SUB,MUL,DOT,AND,OR,NOT,NOTEQ,ASSIGN,EQUAL,LESS,LESSEQ,GREATER,GREATEREQ,CT_INT,CT_REAL,CT_STRING,CT_CHAR};
 
+typedef struct _Token{
+	int code;
+	union{
+		char *text;
+		long int i;
+		double r;
+	};
+	int line;
+	struct _Token *next;
+}Token;
+
+Token *addToken(int code){ //trebuie bagat si numarul liniei, ca si parametru
+	Token *tk;
+	Token *tokens=NULL;
+	Token *lastToken=NULL;
+	SAFEALLOC(tk,Token);
+	tk->code = code;
+	//tk->line = line;
+	tk->next = NULL;
+	if(lastToken){
+		lastToken->next=tk;
+	}
+	else{
+		tokens=tk;
+	}
+	lastToken=tk;
+	return tk;
+}
+
+void err(const char *text,...){
+	va_list va;
+	va_start(va,text);
+	fprintf(stderr,"Eroare: ");
+	vfprintf(stderr,text,va);
+	fputc('\n',stderr);
+	va_end(va);
+	exit(-1);
+}
 int getNextToken(){
 
 	int stare=0;
@@ -11,182 +53,172 @@ int getNextToken(){
 		ch=*caracter;
 		switch(stare){
 			case 0: if(ch==','){
-					s=1;
+					stare=1;
 					caracter++;
 				}
 				else if(ch==';'){
-					s=2;
+					stare=2;
 					caracter++;
 				}
 				else if(ch=='('){
-					s=3;
+					stare=3;
 					caracter++;
 				}
 				else if(ch==')'){
-					s=4;
+					stare=4;
 					caracter++;
 				}
 				else if(ch=='['){
-					s=5;
+					stare=5;
 					caracter++;
 				}
 				else if(ch==']'){
-					s=6;
+					stare=6;
 					caracter++;
 				}
 				else if(ch=='{'){
-					s=7;
+					stare=7;
 					caracter++;
 				}
 				else if(ch=='}'){
-					s=8;
+					stare=8;
 					caracter++;
 				}
 				else if(ch=='+'){
-					s=9;
+					stare=9;
 					caracter++;
 				}
 				else if(ch=='-'){
-					s=10;
+					stare=10;
 					caracter++;
 				}
 				else if(ch=='*'){
-					s=11;
+					stare=11;
 					caracter++;
 				}
 				else if(ch=='.'){
-					s=12;
+					stare=12;
 					caracter++;
 				}
 				else if(ch=='&'){
-					s=13;
+					stare=13;
 					caracter++;
 				}
 				else if(ch=='|'){
-					s=15;
+					stare=15;
 					caracter++;
 				}
 				else if(ch=='!'){
-					s=17;
+					stare=17;
 					caracter++;
 				}
 				else if(ch=='='){
-					s=20;
+					stare=20;
 					caracter++;
 				}
 				else if(ch=='<'){
-					s=23;
+					stare=23;
 					caracter++;
 				}
 				else if(ch=='>'){
-					s=26;
+					stare=26;
 					caracter++;
 				}
+				
 
-				break;
 
-			case 1:	addToken(ID);
-				return ID;
-			case 2: addToken(ID);
-				return ID;
-			case 3:	addToken(ID);
-				return ID;
-			case 4:	addToken(ID);
-				return ID;
-			case 5:	addToken(ID);
-				return ID;			
-			case 6:	addToken(ID);
-				return ID;
-			case 7:	addToken(ID);
-				return ID;
-			case 8:	addToken(ID);
-				return ID;
-			case 9:	addToken(ID);
-				return ID;
-			case 10:addToken(ID);
-				return ID;
-			case 11:addToken(ID);
-				return ID;
-			case 12:addToken(ID);
-				return ID;
+			case 1:	addToken(COMMA);
+				return COMMA;
+			case 2: addToken(SEMICOLON);
+				return SEMICOLON;
+			case 3:	addToken(LPAR);
+				return LPAR;
+			case 4:	addToken(RPAR);
+				return RPAR;
+			case 5:	addToken(LBRACKET);
+				return LBRACKET;			
+			case 6:	addToken(RBRACKET);
+				return RBRACKET;
+			case 7:	addToken(LACC);
+				return LACC;
+			case 8:	addToken(RACC);
+				return RACC;
+			case 9:	addToken(ADD);
+				return ADD;
+			case 10:addToken(SUB);
+				return SUB;
+			case 11:addToken(MUL);
+				return MUL;
+			case 12:addToken(DOT);
+				return DOT;
 			case 13: if(ch=='&'){
-					s=14;
+					stare=14;
 					caracter++;
 				}
-			case 14:addToken(ID)
-				return ID;
+			case 14:addToken(AND);
+				return AND;
 			case 15: if(ch=='|'){
-					s=16;
+					stare=16;
 					caracter++;
 				}
-			case 16:addToken(ID);
-				return ID;
+			case 16:addToken(OR);
+				return OR;
 			case 17: if(ch=='='){
-					s=19;
+					stare=19;
 					caracter++;
 				}
 				else{
-					s=18;
+					stare=18;
 				}
-			case 18:addToken(ID);
-				return ID;
-			case 19:addToken(ID);
-				return ID;
+			case 18:addToken(NOT);
+				return NOT;
+			case 19:addToken(NOTEQ);
+				return NOTEQ;
 			case 20: if(ch=='='){
-					s=22;
+					stare=22;
 					caracter++;
 				}
 				else{
-					s=21;
+					stare=21;
 				}
-			case 21:addToken(ID);
-				return ID;
-			case 22:addToken(ID);
-				return ID;
+			case 21:addToken(ASSIGN);
+				return ASSIGN;
+			case 22:addToken(EQUAL);
+				return EQUAL;
 			case 23: if(ch=='='){
-					s=25;
+					stare=25;
 					caracter++;
 				}
 				else{
-					s=24;
+					stare=24;
 				}
-			case 24:addToken(ID);
-				return ID;
-			case 25:addToken(ID);
-				return ID;
+			case 24:addToken(LESS);
+				return LESS;
+			case 25:addToken(LESSEQ);
+				return LESSEQ;
 			case 26: if(ch=='='){
-					s=28;
+					stare=28;
 					caracter++;
 				}
 				else{
-					s=27;
+					stare=27;
 				}
-			case 27:addToken(ID);
-				return ID;
-			case 28:addToken(ID);
-				return ID;
-
-
-
-
-
-
-
-
-
-
-
-
+			case 27:addToken(GREATER);
+				return GREATER;
+			case 28:addToken(GREATEREQ);
+				return GREATEREQ;
+		}
+	}
 		
 }
 
-int main(int nrArg,char *argument[]){
+int main(){
 	
 	char buffer[30001];
 	int n;
 
 	FILE *fisier;
-	fisier = fopen(argument[1],"r");
+	fisier = fopen("fisier.txt,"r");
 	if(!fisier){
 		printf("Eroare deschidere fisier.\n");
 		exit(1);
@@ -194,7 +226,10 @@ int main(int nrArg,char *argument[]){
 
 	n = fread(buffer,1,30000,fisier);
 	
-
+	while(getNextToken()!=END){
+		
+	
+	}
 	
 
 	
