b6c7f9eafeae6fcd7d769095633ec526ad68740c
St3listu96 <32820467+St3listu96@users.noreply.github.com>
2018-03-22 19:34:54 +0200

    Add files via upload

--- analizor.c
+++ analizor.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 #define SAFEALLOC(var,Type) if((var=(Type*)malloc(sizeof(Type)))==NULL)err("Memorie insuficienta");
 
 char *caracter;
-enum{COMMA,SEMICOLON,LPAR,RPAR,LBRACKET,RBRACKET,LACC,RACC,ADD,SUB,MUL,DOT,AND,OR,NOT,NOTEQ,ASSIGN,EQUAL,LESS,LESSEQ,GREATER,GREATEREQ,CT_INT,CT_REAL,CT_STRING,CT_CHAR};
+enum{COMMA,SEMICOLON,LPAR,RPAR,LBRACKET,RBRACKET,LACC,RACC,ADD,SUB,MUL,DOT,AND,OR,NOT,NOTEQ,ASSIGN,EQUAL,LESS,LESSEQ,GREATER,GREATEREQ,CT_INT,CT_REAL,CT_STRING,CT_CHAR,END,BREAK,CHAR,DOUBLE,ELSE,FOR,IF,INT,RETURN,STRUCT,VOID,WHILE,ID};
 
 typedef struct _Token{
 	int code;
@@ -18,11 +19,11 @@ typedef struct _Token{
 	struct _Token *next;
 }Token;
 
-Token *addToken(int code){ //trebuie bagat si numarul liniei, ca si parametru
+Token *addToken(int code){
 	Token *tk;
 	Token *tokens=NULL;
 	Token *lastToken=NULL;
-	SAFEALLOC(tk,Token);
+	SAFEALLOC(tk,Token)
 	tk->code = code;
 	//tk->line = line;
 	tk->next = NULL;
@@ -48,85 +49,123 @@ void err(const char *text,...){
 int getNextToken(){
 
 	int stare=0;
+	int lungimeCuvant;
 	char ch;
+	const char *startCh;
+	Token *tk;
+	int line=1;
 	while(1){
 		ch=*caracter;
+		printf("#%d %c(%d)\n",stare,ch,ch);
 		switch(stare){
 			case 0: if(ch==','){
-					stare=1;
 					caracter++;
+					stare=1;
 				}
 				else if(ch==';'){
-					stare=2;
 					caracter++;
+					stare=2;
 				}
 				else if(ch=='('){
-					stare=3;
 					caracter++;
+					stare=3;
 				}
 				else if(ch==')'){
-					stare=4;
 					caracter++;
+					stare=4;
 				}
 				else if(ch=='['){
-					stare=5;
 					caracter++;
+					stare=5;
 				}
 				else if(ch==']'){
-					stare=6;
 					caracter++;
+					stare=6;
 				}
 				else if(ch=='{'){
-					stare=7;
 					caracter++;
+					stare=7;
 				}
 				else if(ch=='}'){
-					stare=8;
 					caracter++;
+					stare=8;
 				}
 				else if(ch=='+'){
-					stare=9;
 					caracter++;
+					stare=9;
 				}
 				else if(ch=='-'){
-					stare=10;
 					caracter++;
+					stare=10;
 				}
 				else if(ch=='*'){
-					stare=11;
+					
 					caracter++;
+					stare=11;
 				}
 				else if(ch=='.'){
-					stare=12;
+					
 					caracter++;
+					stare=12;
 				}
 				else if(ch=='&'){
-					stare=13;
+					
 					caracter++;
+					stare=13;
 				}
 				else if(ch=='|'){
-					stare=15;
+					
 					caracter++;
+					stare=15;
 				}
 				else if(ch=='!'){
-					stare=17;
+					
 					caracter++;
+					stare=17;
 				}
 				else if(ch=='='){
-					stare=20;
+					
 					caracter++;
+					stare=20;
 				}
 				else if(ch=='<'){
-					stare=23;
+					
 					caracter++;
+					stare=23;
 				}
 				else if(ch=='>'){
-					stare=26;
+					
 					caracter++;
+					stare=26;
+				}
+				else if(isalpha(ch)||ch=='_'){
+					
+					caracter++;
+					stare=29;
 				}
 				
-
-
+				else if(ch=='\n'){
+					line++;
+					caracter++;
+				}
+				else if(ch==0){
+					addToken(END);
+					return END;
+				}
+				else if(ch==' '||ch=='\r'||ch=='\t'){
+					caracter++;
+				}
+				else if(ch>='1'&&ch<='9'){
+					caracter++;
+					stare=31;
+				}
+				else if(ch=='0'){
+					caracter++;
+					stare=32;
+				}
+				break;
+				
+			
 			case 1:	addToken(COMMA);
 				return COMMA;
 			case 2: addToken(SEMICOLON);
@@ -158,14 +197,16 @@ int getNextToken(){
 			case 14:addToken(AND);
 				return AND;
 			case 15: if(ch=='|'){
-					stare=16;
+					
 					caracter++;
+					stare=16;
 				}
 			case 16:addToken(OR);
 				return OR;
 			case 17: if(ch=='='){
-					stare=19;
+					
 					caracter++;
+					stare=19;
 				}
 				else{
 					stare=18;
@@ -175,8 +216,9 @@ int getNextToken(){
 			case 19:addToken(NOTEQ);
 				return NOTEQ;
 			case 20: if(ch=='='){
-					stare=22;
+					
 					caracter++;
+					stare=22;
 				}
 				else{
 					stare=21;
@@ -186,8 +228,9 @@ int getNextToken(){
 			case 22:addToken(EQUAL);
 				return EQUAL;
 			case 23: if(ch=='='){
-					stare=25;
+					
 					caracter++;
+					stare=25;
 				}
 				else{
 					stare=24;
@@ -197,8 +240,9 @@ int getNextToken(){
 			case 25:addToken(LESSEQ);
 				return LESSEQ;
 			case 26: if(ch=='='){
-					stare=28;
+					
 					caracter++;
+					stare=28;
 				}
 				else{
 					stare=27;
@@ -207,6 +251,170 @@ int getNextToken(){
 				return GREATER;
 			case 28:addToken(GREATEREQ);
 				return GREATEREQ;
+			case 29: if(isalnum(ch)||ch=='_'){
+						startCh=caracter;
+						caracter++;
+						stare=29;
+					}
+					else{
+						stare=30;
+					}
+			case 30: lungimeCuvant=caracter-startCh;
+					if(lungimeCuvant==5 && !memcmp(startCh,"break",5))
+						tk=addToken(BREAK);
+					else if(lungimeCuvant==4 && !memcmp(startCh,"char",4))
+						tk=addToken(CHAR);
+					else if(lungimeCuvant==6 && !memcmp(startCh,"double",6))
+						tk=addToken(DOUBLE);
+					else if(lungimeCuvant==4 && !memcmp(startCh,"else",4))
+						tk=addToken(ELSE);
+					else if(lungimeCuvant==3 && !memcmp(startCh,"for",3))
+						tk=addToken(FOR);
+					else if(lungimeCuvant==2 && !memcmp(startCh,"if",2))
+						tk=addToken(IF);
+					else if(lungimeCuvant==3 && !memcmp(startCh,"int",3))
+						tk=addToken(INT);
+					else if(lungimeCuvant==6 && !memcmp(startCh,"return",6))
+						tk=addToken(RETURN);
+					else if(lungimeCuvant==6 && !memcmp(startCh,"struct",6))
+						tk=addToken(STRUCT);
+					else if(lungimeCuvant==4 && !memcmp(startCh,"void",4))
+						tk=addToken(VOID);
+					else if(lungimeCuvant==5 && !memcmp(startCh,"while",5))
+						tk=addToken(WHILE);
+					else{
+						addToken(ID);
+						//tk->text = creezaString(startCh,caracter);
+					}
+					return tk->code;
+					break;
+			
+			case 31 : if(isdigit(ch)){
+						caracter++;
+						stare=31;
+					}
+					else if(ch=='.'){
+						caracter++;
+						stare=39;
+					}
+					else if(ch=='e' || ch=='E'){
+						caracter++;
+						stare=41;
+					}
+					else{
+						stare=36;
+					}
+					break;
+			
+			case 32: if(ch=='x'){
+						caracter++;
+						stare=34;		
+					}
+					else{
+						stare=33;
+					}
+					break;
+					
+			case 33: if(ch>='0'&&ch<='7'){
+						caracter++;
+						stare=33;
+					}
+					else if(ch=='8' || ch=='9'){
+						caracter++;
+						stare=37;
+					}
+					else if(ch=='.'){
+						caracter++;
+						stare=39;
+					}
+					else if(ch=='e' || ch=='E'){
+						caracter++;
+						stare=41;
+					}
+					else{
+						stare=36;
+					}
+					break;
+					
+			case 34: if(isdigit(ch) || (ch>='a' && ch <='f') || (ch>='A' && ch <= 'F')){
+						caracter++;
+						stare=35;
+					}
+					break;
+					
+			case 35: if(isdigit(ch) || (ch>='a' && ch <='f') || (ch>='A' && ch <= 'F')){
+						caracter++;
+						stare=35;
+					}
+					else{
+						stare=36;
+					}
+					break;
+				
+			case 36: addToken(CT_INT);
+					return CT_INT;
+					break;
+			
+			case 37: if(isdigit(ch)){
+						caracter++;
+						stare=37;
+					}
+					else if(ch=='.'){
+						caracter++;
+						stare=39;
+					}
+					else if(ch=='e' || ch =='E'){
+						caracter++;
+						stare=41;
+					}
+					break;
+					
+			case 39: if(isdigit(ch)){
+						caracter++;
+						stare=40;
+					}
+					break;
+					
+			case 40: if(isdigit(ch)){
+						caracter++;
+						stare=40;
+					}
+					else if(ch=='e' || ch=='E'){
+						caracter++;
+						stare=41;
+					}
+					break;
+					
+			case 41: if(ch=='+' || ch=='-'){
+						caracter++;
+						stare=42;
+					}
+					else{
+						stare=42;
+					}
+					break;
+					
+			case 42: if(isdigit(ch)){
+						caracter++;
+						stare=43;
+					}
+					break;
+					
+			case 43: if(isdigit(ch)){
+						caracter++;
+						stare=43;
+					}
+					else{
+						stare=44;
+					}
+					break;
+					
+			case 44: addToken(CT_REAL);
+					return CT_REAL;
+					
+			default: printf("Stare inexistenta.\n");
+					break;
+					
 		}
 	}
 		
@@ -218,24 +426,19 @@ int main(){
 	int n;
 
 	FILE *fisier;
-	fisier = fopen("fisier.txt,"r");
+	fisier = fopen("8.c","r");
 	if(!fisier){
 		printf("Eroare deschidere fisier.\n");
 		exit(1);
 	}
-
 	n = fread(buffer,1,30000,fisier);
-	
+	buffer[n]='\0';
+	caracter=buffer;
 	while(getNextToken()!=END){
 		
 	
 	}
 	
-
 	
-
-
-
-
 	return 0;
 }
