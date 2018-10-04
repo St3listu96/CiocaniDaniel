7c59ecc3773e6f79bb5a45218d21995d16445e36
St3listu96 <32820467+St3listu96@users.noreply.github.com>
2018-10-04 19:25:44 +0300

    Add files via upload

---
+++ DaniCiocani/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DaniCiocani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxIV = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DES",
+            "3DES",
+            "RC2",
+            "AES",
+            "Rijndael"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate IV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(204, 68);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(132, 20);
+            this.textBoxKey.TabIndex = 3;
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxIV
+            // 
+            this.textBoxIV.Location = new System.Drawing.Point(205, 104);
+            this.textBoxIV.Name = "textBoxIV";
+            this.textBoxIV.Size = new System.Drawing.Size(130, 20);
+            this.textBoxIV.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Encrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Encrypt);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Decrypt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_Decrypt);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 302);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxIV);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxIV;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ DaniCiocani/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DaniCiocani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        SymmetricAlgorithm mySymmetricAlg;
+        ConversionHandler myConverter = new ConversionHandler();
+
+        public void GenerateKey_2(string cipher)
+        {
+            switch (cipher)
+            {
+                case "DES":
+                    mySymmetricAlg = DES.Create();
+                    break;
+                case "3DES":
+                    mySymmetricAlg = TripleDES.Create();
+                    break;
+                case "Rijndael":
+                    mySymmetricAlg = Rijndael.Create();
+                    break;
+                case "RC2":
+                    mySymmetricAlg = RC2.Create();
+                    break;
+                case "AES":
+                    mySymmetricAlg = Aes.Create();
+                    break;
+            }
+            mySymmetricAlg.GenerateKey();
+        }
+
+        public void GenerateIV_2(string cipher)
+        {
+            switch (cipher)
+            {
+                case "DES":
+                    mySymmetricAlg = DES.Create();
+                    break;
+                case "3DES":
+                    mySymmetricAlg = TripleDES.Create();
+                    break;
+                case "Rijndael":
+                    mySymmetricAlg = Rijndael.Create();
+                    break;
+                case "RC2":
+                    mySymmetricAlg = RC2.Create();
+                    break;
+                case "AES":
+                    mySymmetricAlg = Aes.Create();
+                    break;
+            }
+            mySymmetricAlg.GenerateIV();
+        }
+
+        public byte[] Encrypt(byte[] mess, byte[] key, byte[] iv)
+         {
+         mySymmetricAlg.Key = key;
+         mySymmetricAlg.IV = iv;
+         MemoryStream ms = new MemoryStream();
+         CryptoStream cs = new CryptoStream(ms,mySymmetricAlg.CreateEncryptor(),CryptoStreamMode.Write);
+         cs.Write(mess, 0, mess.Length);
+         cs.Close();
+         return ms.ToArray();
+         }
+
+        public byte[] Decrypt(byte[] mess, byte[] key, byte[] iv)
+         {
+         byte[] plaintext = new byte[mess.Length];
+         mySymmetricAlg.Key = key;
+         mySymmetricAlg.IV = iv;
+         MemoryStream ms = new MemoryStream(mess);
+         CryptoStream cs = new CryptoStream(ms,mySymmetricAlg.CreateDecryptor(),CryptoStreamMode.Read);
+         cs.Read(plaintext, 0, mess.Length);
+         cs.Close();
+         return plaintext;
+         }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateKey_2(comboBox1.Text);
+            textBoxKey.Text = ByteArrayToString(mySymmetricAlg.Key);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GenerateIV_2(comboBox1.Text);
+            textBoxIV.Text = ByteArrayToString(mySymmetricAlg.IV);
+        }
+
+        private void btn_Encrypt(object sender, EventArgs e)
+        {
+            byte[] ciphertext = Encrypt(myConverter.StringToByteArray(textBox1.Text),myConverter.HexStringToByteArray(textBoxKey.Text),myConverter.HexStringToByteArray(textBoxIV.Text));
+            textBox2.Text = myConverter.ByteArrayToHexString(ciphertext);
+           
+        }
+
+        private void btn_Decrypt(object sender, EventArgs e)
+        {
+            byte[] plaintext =Decrypt(myConverter.HexStringToByteArray(textBox2.Text),myConverter.HexStringToByteArray(textBoxKey.Text),myConverter.HexStringToByteArray(textBoxIV.Text));
+            textBox1.Text = myConverter.ByteArrayToString(plaintext);
+            
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
